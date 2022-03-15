14a51c790fca99932b9a02cf82213ec6f1ecc269
Tunisia Artope <artope@yahoo.com>
2022-03-15 11:17:47 -0400

    commit tree 49e0500c49d4a8115aa4758bb8721053d475b90f

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+John = Person.new
+John.talk
+John.walk
